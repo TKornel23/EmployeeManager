cd9abefde2cfca3c08a30a5ae76aa2355d2f4ad6
TKornel23 <tkornel23@gmail.com>
2022-01-16 21:08:09 +0100

    prepared for backend api

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,9 @@ import { EmployeesComponent } from './employees/employees.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/modal-post/modal-post.component.ts
+++ client/src/app/modal-post/modal-post.component.ts
@@ -1,15 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-post',
   templateUrl: './modal-post.component.html',
   styleUrls: ['./modal-post.component.css']
 })
+
 export class ModalPostComponent implements OnInit {
 
-  constructor() { }
+  employee = new FormGroup({
+    email: new FormControl(),
+    name: new FormControl(),
+    address: new FormControl(),
+    city: new FormControl(),
+    zip: new FormControl(),
+    dateOfBirth: new FormControl(),
+    Gender: new FormControl()
+  });
+
+  constructor(/*private http: HttpClient*/) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    /*const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+    this.http.post("", JSON.stringify(employee), httpOptions).subscribe(response => {
+      console.log(response);
+    },
+    {
+      error => {
+        console.log(error);
+      }
+    });
+    */
   }
 
 }
